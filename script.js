739f5e8b916167b22bcdc85e5d05ef1f8aa6d2a5
ZeniaMed <zeniakapoorofficial@gmail.com>
2025-03-15 17:58:19 +0530

    Update script.js
    Signed-off-by: ZeniaMed <zeniakapoorofficial@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("ZeniaMed App Loaded Successfully!");
+document.querySelectorAll(".category-btn").forEach(button => {
+    button.addEventListener("click", function () {
+        alert("Redirecting to category: " + this.innerText);
+    });
 });
